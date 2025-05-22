7d9e4fdfef1a13a9238b85cb8b021032040d1878
Saksham Khatwani <sakshamkhatwani@Samarths-MacBook-Pro.local>
2025-05-22 02:10:23 -0600

    updated the graphDataset

--- GraphDataset.py
+++ GraphDataset.py
@@ -3,6 +3,7 @@ import torch
 import os
 from torch_geometric.data import Data, Dataset, download_url
 import jsonpickle
+from Preprocess import create_graph_penman
 
 class MyOwnDataset(Dataset):
     def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
@@ -10,12 +11,12 @@ class MyOwnDataset(Dataset):
 
     @property
     def raw_file_names(self):
-        return ['doc_aligned_with_joined_sentence.json']
+        return ['combined_data_annoted.json']
 
     @property
     def processed_file_names(self):
-        return os.listdir(self.processed_dir)
-        # return "no.pt"
+        # return os.listdir(self.processed_dir)
+        return "no.pt"
 
     def download(self):
         pass
@@ -27,7 +28,8 @@ class MyOwnDataset(Dataset):
             "endeavor": 2,
             "habitual": 3,
             "state": 4,
-            "activity": 5
+            "activity": 5,
+            "none": 6
         }
 
         idx = 0
@@ -41,10 +43,10 @@ class MyOwnDataset(Dataset):
                   # print("here", i)
                   # original_string, graph_str, var_mapping
                   # print("data", data[i])
-                  print("raw_path", data[i])
-                  graph = create_graph(data[i]['joined_sentence'], data[i]['graph'], data[i]['mapping'], data[i]['variable'])
+                  # print("raw_path", data[i])
+                  # graph = create_graph(data[i]['joined_sentence'], data[i]['graph'], data[i]['mapping'], data[i]['variable'])
                   # print("got graph")
-                  labels = F.one_hot(torch.tensor(labelMapping[data[i]['aspect']] if data[i]['aspect'] in labelMapping.keys() else 0), num_classes=6)
+                  labels = F.one_hot(torch.tensor(labelMapping[data[i]['aspect']] if data[i]['aspect'] in labelMapping.keys() else 0), num_classes=7)
                   # labels[labelMapping[data[i]['aspect']] if data[i]['aspect'] in labelMapping.keys() else 0] = 1
 
                   # 0 -> aspect
@@ -55,7 +57,8 @@ class MyOwnDataset(Dataset):
                   labels = torch.cat((labels, torch.tensor([1 if data[i]['aspect'] == "performance" or data[i]['aspect'] == "endeavor" else 0])), dim=0)
                   labels = torch.cat((labels, torch.tensor([1 if data[i]['aspect'] == "performance" else 0])), dim=0)
 
-                  dataGraph = create_data_graph(graph[0], graph[1], graph[2], graph[4], graph[3], labels)
+                  # dataGraph = create_data_graph(graph[0], graph[1], graph[2], graph[4], graph[3], labels)
+                  dataGraph = create_graph_penman(data[i]['graph'], data[i]['mapping'], data[i]['variable'], labels)
 
                   torch.save(dataGraph, os.path.join(self.processed_dir, f'data_{idx}.pt'))
                   idx += 1
@@ -68,3 +71,6 @@ class MyOwnDataset(Dataset):
     def get(self, idx):
         data = torch.load(os.path.join(self.processed_dir, f'data_{idx}.pt'), weights_only=False)
         return data
+
+if __name__ == "__main__":
+    dataset = MyOwnDataset(root="./UMRDataset")
--- Preprocess.py
+++ Preprocess.py
@@ -5,216 +5,129 @@ from sklearn.metrics.pairwise import cosine_similarity
 import re
 import matplotlib.pyplot as plt
 from collections import defaultdict
-from transformers import BertModel, BertTokenizer
+from transformers import AutoTokenizer, AutoModel
 from utils.HelperFunctions import get_embeddings_with_pos
+from sklearn.model_selection import train_test_split
+import jsonpickle
+import penman
+# from GraphDataset import MyOwnDataset
 
-def clean_graph(graph_str):
-    #1:clean graph takes in a string, strips the white space aroung it, makes consistent indentations
-    #2:changes all ': text' to start on its own line for the sake of graph making
-    graph_str = graph_str.rstrip()
 
-    indent = 4 #just pick an indentation level, will fix after everything is on a new line
+MODEL_NAME = 'google-bert/bert-large-uncased'
+MODEL_DIM = 1024
 
-    #1. consistent indentation- everything will be 4 spaces
-    head_spaces = {}
-    spacings = []
-    all_lines = graph_str.splitlines()
-    #start by retrieving all spaces
-    for line in all_lines:
-        dist = len(line) - len(line.lstrip())
-        spacings.append(dist)
-    # print(spacings)
-    #Create new spacings
-    no_dupes = []
-    [no_dupes.append(x) for x in spacings if x not in no_dupes]
-    # print(no_dupes)
-    new_spacings = [sorted(no_dupes).index(x) * 4 for x in spacings]
+def split_data(df, target_column, train_size=0.7, random_state=None):
+    train_df, test_df = train_test_split(
+        df, train_size=train_size, stratify=df[target_column], random_state=random_state
+    )
+    return train_df, test_df
 
-    #switch out the spacings
-    for line_i in range(len(all_lines)):
-        all_lines[line_i]=new_spacings[line_i] *' ' + all_lines[line_i].lstrip()
-    graph_str = '\n'.join(all_lines)
+def get_embeddings_with_pos(text):
+  tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+  model = AutoModel.from_pretrained(MODEL_NAME)
 
-    #2: splits :x to be its own line
-    lines= re.findall(r'.*:.*:.*',graph_str)# find lines with multiple ":"
-    for line in lines:
-        #actually does the replacement on new level
-        line = line.split(':')
-        for i in range(2,len(line)):
-            num_spaces = len(line[0]) - len(line[0].lstrip())
-            new_str = '\n' + ' ' * num_spaces + indent * ' ' + ":"
-            graph_str = re.sub(r'(?<=[^\s]) :', new_str,graph_str,count = 1)
+  embeddings = []
 
-    return graph_str
+  num_tokens = []
 
-def extract_node_2(node_str, i):
-  node_str = re.sub(r'\"*_\(.*\)"', '"', node_str)
-  node_str = node_str.strip()
-  node_list = node_str.split('(')
-  # print(node_str)
-  if len(node_list) == 1:
-    #it can either end or not depending on )
-    if node_list[0][-1] == ")":
-      # print("condition 1")
-      edge = node_list[0].split(")")[0].split()[0].strip()
-      g_id = node_list[0].split(")")[0].split()[1].strip()
-      node = g_id
+  for word in text.split():
+    num_tokens.append(len(tokenizer.tokenize(word)))
+    
+  tokenized = tokenizer(text, truncation=True, return_tensors="pt")
+  embedded_input = model(**tokenized)
 
-      return node, g_id, edge, 'remove', node_list[0].count(")")
+  curr = 1
+  for i in range(len(num_tokens)):
+    embeddings.append(torch.sum(embedded_input.last_hidden_state[0][curr:curr+num_tokens[i]], dim=0))
+    curr += num_tokens[i]
 
-    else:
-      # print("condition 2", node_list[0])
+  embeddings = torch.stack(embeddings)
 
-      edge = node_list[0].split()[0].strip()
-      g_id = node_list[0].split()[1].strip() if len(node_list[0].split()) > 1 else None
-      node = g_id
+  return embeddings
 
-      return node, g_id, edge, 'none', node_list[0].count(")")
+def create_graph_penman(graph_str, var_mapping, target, label):
+  embeddings_with_pos = get_embeddings_with_pos(graph_str)
 
-  else:
-    if node_list[0] == '':
-      # print("condition 3")
-      g_id = node_list[1].split("/")[0].strip()
-      node = node_list[1].split("/")[1].strip()
-      edge = None
+  penman_graph = penman.decode(graph_str)
 
-      return node, g_id, edge, 'add', node_list[1].count(")") - 1
-
-    elif node_list[1][-1] == ')':
-      # print("condition 4")
-      edge = node_list[0].strip()
-      g_id = node_list[1].split(")")[0].split("/")[0].strip()
-      node = node_list[1].split(")")[0].split("/")[1].strip()
-
-      return node, g_id, edge, 'none', node_list[1].count(")") - 1
-    else:
-      # print("condition 5")
-      g_id = node_list[1].split("/")[0].strip()
-      node = node_list[1].split("/")[1].strip()
-      edge = node_list[0].strip()
-
-      return node, g_id, edge, 'add', node_list[1].count(")") - 1
-
-def add_node_to_graph(index_to_node_dict, node_to_index_dict, g_id):
-  if g_id not in node_to_index_dict.keys():
-    node_to_index_dict[g_id] = len(node_to_index_dict)
-    index_to_node_dict[len(index_to_node_dict)] = g_id
-
-def create_graph(original_string, graph_str, var_mapping, target):
-    # embeddings_with_pos = get_embeddings_with_pos(original_string)
-
-    head_dict = {}
-
-    parent_child_dict = defaultdict(list)
-    node_to_index_dict = {}
-    index_to_node_dict = {}
-    index_to_embedding = {}
-    is_leaf = {}
-
-    # model_name = 'bert-base-uncased'
-
-    # tokenizer = BertTokenizer.from_pretrained(model_name)
-
-    found = 0
-    train_mask_index = -1
-    graph_str = clean_graph(graph_str)
-
-    graph_stack = []
-
-    for i in range(len(graph_str.splitlines())):
-        #extact node and edge, get spacing to know what to attach it to
-        curr_line = graph_str.splitlines()[i]
-
-        node, g_id, edge, operation, num_remove = extract_node_2(curr_line,i)
-        # print(node, g_id, edge, operation, num_remove)
-        # print(graph_stack)
-        if g_id == None or g_id == '':
-          continue
-
-        if g_id != node and g_id not in var_mapping.keys():
-          var_mapping[g_id] = [0, 0]
-
-        if operation == 'add':
-          if edge == None:
-            add_node_to_graph(index_to_node_dict, node_to_index_dict, g_id)
-            graph_stack.append(g_id)
-
-          else:
-            if g_id in var_mapping.keys() or (edge == ':aspect' and node == 'blank' and graph_stack[-1] == target):
-              add_node_to_graph(index_to_node_dict, node_to_index_dict, g_id)
-
-              parent_child_dict[node_to_index_dict[graph_stack[-1]]].append(node_to_index_dict[g_id])
-              graph_stack.append(g_id)
-
-        elif operation == 'remove':
-          if g_id in var_mapping.keys() or (edge == ':aspect' and node == 'blank' and graph_stack[-1] == target):
-            add_node_to_graph(index_to_node_dict, node_to_index_dict, g_id)
-            parent_child_dict[node_to_index_dict[graph_stack[-1]]].append(node_to_index_dict[g_id])
-
-        else:
-          if g_id in var_mapping.keys() or (edge == ':aspect' and node == 'blank' and graph_stack[-1] == target):
-            add_node_to_graph(index_to_node_dict, node_to_index_dict, g_id)
-            parent_child_dict[node_to_index_dict[graph_stack[-1]]].append(node_to_index_dict[g_id])
-
-        if g_id in var_mapping.keys():
-          left_end = var_mapping[g_id][0]
-          right_end = var_mapping[g_id][1]
-
-          if left_end == 0:
-            # index_to_embedding[node_to_index_dict[g_id]] = torch.zeros(768)
-            # index_to_embedding[node_to_index_dict[g_id]] = {"input_ids": torch.tensor([101, 103, 102]).unsqueeze(0), "attention_mask": torch.tensor([1]).unsqueeze(0)}
-            # index_to_embedding[node_to_index_dict[g_id]] = tokenizer("[MASK]", truncation=True, return_tensors="pt", max_length=20, padding='max_length')
-            index_to_embedding[node_to_index_dict[g_id]] = "[MASK]"
-          else:
-            # index_to_embedding[node_to_index_dict[g_id]] = torch.sum(embeddings_with_pos[left_end - 1:right_end], dim=0)
-            # index_to_embedding[node_to_index_dict[g_id]] = tokenizer(original_string[left_end - 1:right_end], truncation=True, return_tensors="pt", max_length=20, padding='max_length')
-            index_to_embedding[node_to_index_dict[g_id]] = original_string[left_end - 1:right_end]
-          var_mapping[g_id] = [0, 0]
-
-        if (edge == ':aspect' and node == 'blank' and graph_stack[-1] == target):
-          # index_to_embedding[node_to_index_dict[g_id]] = torch.zeros(768)
-          # index_to_embedding[node_to_index_dict[g_id]] = {"input_ids": torch.tensor([101, 103, 102]).unsqueeze(0), "attention_mask": torch.tensor([1]).unsqueeze(0)}
-          # index_to_embedding[node_to_index_dict[g_id]] = tokenizer("[MASK]", truncation=True, return_tensors="pt", max_length=20, padding='max_length')
-          index_to_embedding[node_to_index_dict[g_id]] = "[MASK]"
-          train_mask_index = node_to_index_dict[g_id]
-
-        for j in range(num_remove):
-          graph_stack.pop()
-
-    return parent_child_dict, node_to_index_dict, index_to_node_dict, index_to_embedding, train_mask_index
-
-def create_data_graph(parent_child_dict, node_to_index_dict, index_to_node_dict, train_mask_index, embeddings, label):
-
-  model_name = 'bert-base-uncased'
-
-  tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+  instances = penman_graph.instances()
+  edges = penman_graph.edges()
 
   node_data = torch.tensor([])
-  node_input_ids = torch.tensor([])
-  node_attention_mask = torch.tensor([])
-  # edges = [[0 for i in range(len(embeddings.keys()))] for i in range(len(embeddings.keys()))]
-  edges = []
-  train_mask = torch.zeros(len(embeddings))
-  train_mask[train_mask_index] = 1
 
-  for k in sorted(index_to_node_dict.keys()):
-    temp = embeddings[k]
-    node_tokens = torch.tensor([])
-    # for child in parent_child_dict[k]:
-    #   temp = temp + embeddings[child]
-    #   edges.append([k, child])
+  node2index = {}
+  index2node = {}
+  curr_index = 0
 
-    for child in parent_child_dict[k]:
-      # print()
-      # temp.append(embeddings[child])
-      temp = temp + '[SEP]' + embeddings[child]
-      edges.append([k, child])
+  edges_rec = []
 
-    node_tokens = tokenizer(temp, truncation=True, return_tensors="pt", max_length=256, padding='max_length')
-    node_input_ids = torch.cat((node_input_ids, node_tokens['input_ids']), dim=0)
-    node_attention_mask = torch.cat((node_attention_mask, node_tokens['attention_mask']), dim=0)
-    # print(node_tokens)
+  target_index = -1
+  # if g_id in var_mapping.keys():
 
-    # node_data = torch.cat((node_data, temp.unsqueeze(0)), dim=0)
+  for i in range(len(instances)):
+    s = instances[i].source
 
-  return Data(x=node_input_ids, edge_index=torch.tensor(edges).t().contiguous(), y=torch.tensor(label), train_mask=train_mask, input_ids=node_input_ids, attention_mask=node_attention_mask)
+    if s not in var_mapping.keys():
+      var_mapping[s] = [[0, 0]]
+    indices = var_mapping[s]
+    # print("got indices", indices)
+    temp = torch.zeros(embeddings_with_pos.shape[-1])
+
+    for j in indices:
+      if s not in node2index.keys():
+        node2index[s] = curr_index
+        index2node[curr_index] = s
+        curr_index += 1
+
+      if j[0] != 0:
+        temp += torch.sum(embeddings_with_pos[j[0] - 1:j[0]], dim=0)
+
+    node_data = torch.cat((node_data, temp.unsqueeze(0)), dim=0)
+
+    if s == target:
+      target_index = node2index[s]
+      node_data = torch.cat((node_data, torch.zeros(embeddings_with_pos.shape[-1]).unsqueeze(0)), dim=0)
+      node2index['aspect'] = curr_index
+      index2node[curr_index] = 'aspect'
+      curr_index += 1
+
+
+  for edge in edges:
+    source = edge.source
+    target = edge.target
+    edges_rec.append([node2index[source], node2index[target]])
+
+  edges_rec.append([target_index, node2index['aspect']])
+  edges_data = torch.tensor(edges_rec).t().contiguous()
+
+  return Data(x=node_data, edge_index=edges_data, y=torch.tensor(label), target_node=torch.tensor(node2index['aspect']))
+
+def createAlignedDataset(dataDir):
+  doc_aligned_with_joined_sentence = open(dataDir + "/combined_data_annoted.json", 'w')
+
+  with open(dataDir + "/combined_all.json", 'r') as f:
+      data = jsonpickle.decode(f.read())
+      print("data is", f.read())
+      for i in range(len(data)):
+        data[i]['joined_sentence'] = ' '.join(data[i]['sentence'].values())
+        data[i]['mapping'] = {}
+
+        for item in data[i]['alignment']:
+          key = item.split(":")[0]
+          items = item.split(":")[1].strip().split(',')
+          mappings = []
+
+          for item in items:
+            item = item.split("-")
+            mappings.append([int(item[0]), int(item[-1])])
+
+          data[i]['mapping'][key] = mappings
+
+      doc_aligned_with_joined_sentence.write(jsonpickle.encode(data))
+
+  doc_aligned_with_joined_sentence.close()
+
+if __name__ == "__main__":
+  dataDir = "./UMR Data"
+  # createAlignedDataset(dataDir)
+  # dataset = MyOwnDataset(root=dataDir)
